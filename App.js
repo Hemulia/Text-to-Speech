b317808de06ee3ec4cf0690675ef56719aff95e1
Emilia Mäkelä <bgr887@myy.haaga-helia.fi>
2023-03-13 01:08:28 +0200

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+  const [text, setText] = useState('');
+
+  const handleSpeak = () => {
+    Speech.speak(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Kirjoita teksti:</Text>
+      <TextInput style={styles.input} value={text} onChangeText={setText} />
+      <TouchableOpacity style={styles.button} onPress={handleSpeak}>
+        <Text style={styles.buttonText}>Lue ääneen</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +23,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 8,
+    width: '80%',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
   },
 });
